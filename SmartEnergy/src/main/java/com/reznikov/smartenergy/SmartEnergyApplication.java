0ca212b4bdbf44273f8321e245182678dc42a1e6
MaxFrayd <maxfrayd@gmail.com>
2024-07-17 18:20:44 +0200

    Config done

--- SmartEnergy/src/main/java/com/reznikov/smartenergy/SmartEnergyApplication.java
+++ SmartEnergy/src/main/java/com/reznikov/smartenergy/SmartEnergyApplication.java
@@ -2,8 +2,10 @@ package com.reznikov.smartenergy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SmartEnergyApplication {
 
     public static void main(String[] args) {
