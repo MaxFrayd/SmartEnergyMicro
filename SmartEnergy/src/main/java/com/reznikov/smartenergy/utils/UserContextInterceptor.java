5e3f34ab86c2abe02ef9ffa07356036dc11a5a41
MaxFrayd <maxfrayd@gmail.com>
2024-09-11 17:39:44 +0200

    security finished fixed

--- SmartEnergy/src/main/java/com/reznikov/smartenergy/utils/UserContextInterceptor.java
+++ SmartEnergy/src/main/java/com/reznikov/smartenergy/utils/UserContextInterceptor.java
@@ -1,7 +1,5 @@
 package com.reznikov.smartenergy.utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
